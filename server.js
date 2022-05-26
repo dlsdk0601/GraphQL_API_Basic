1e172a5ad1298abf19e18d86e6c0acb9b26ee608
dlsdk0601 <dlsdk0601@gmail.com>
2022-05-26 23:47:16 +0900

    typeDefs nullabled field and mutation type setting

--- server.js
+++ server.js
@@ -15,9 +15,20 @@ const typeDefs = gql`
     author: User
   }
 
+  # GET을 위한 타입 설정
+  # 가장 기본적인 Type이므로 이게 없으면 서버가 실행되지않음
   type Query {
-    allTweets: [Tweet]
-    tweet(id: ID): Tweet
+    # !를 쓰게되면 nullunabled field가 된다. 즉, null이 반환되게 되면 error를 일으킴
+    allTweets: [Tweet!]!
+
+    # id 파라미터에 ! 적으면 필수값이 된다
+    tweet(id: ID!): Tweet
+  }
+
+  # Mutaion 을 위한 타입 설정
+  type Mutation {
+    postTweet(text: String, userId: ID): Tweet
+    deleteTweet(id: ID): Boolean
   }
 `;
 
